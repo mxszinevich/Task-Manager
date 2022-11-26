4193f36f15a326ee34add6678f12773b9a759b8f
mxszinevich <maksim.zinevich@bk.ru>
2022-11-27 01:50:06 +0300

    fix(style): Правки isort

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,5 +1,6 @@
+"""isort:skip_file"""
 from .base import Base
 from .category import Category
-from .task import Task
 from .task_category import TaskCategory
+from .task import Task
 from .user import User
